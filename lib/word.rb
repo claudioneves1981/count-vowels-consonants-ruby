a8b5ab1b650dbecbc95f6aacf80e50bb20a2968d
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-27 17:00:49 -0300

    Desafio Count Vowels Consonants Ruby

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,10 @@
 class Word
+
   def vowels_count(phrase)
+    phrase.count "aeiouyáéíóúãõAEIOUÁÉÍÓÚÃÕY"
   end
 
   def consonants_count(phrase)
+    phrase.count "^aeiouyáéíóúãõAEIOUÁÉÍÓÚÃÕY .,"
   end
 end
